691efafa5cbeb8fa92fca1fe2abc57ba948e979e
Felix Gehring <quantal-quetzal>
2023-10-02 12:35:49 +0200

    💚

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,6 @@
 import { Success } from "./Success";
 
-export async function Page() {
+async function Page() {
   return (
     <div className="bg-slate-200 dark:bg-slate-800">
       <main className="container p-2">
